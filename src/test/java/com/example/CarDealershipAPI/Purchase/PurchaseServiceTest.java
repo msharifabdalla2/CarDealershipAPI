ec9937f764d5082d39f5c3af865daaff682c519b
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-01-29 13:50:28 +0800

    Update PurchaseServiceTest.java

--- src/test/java/com/example/CarDealershipAPI/Purchase/PurchaseServiceTest.java
+++ src/test/java/com/example/CarDealershipAPI/Purchase/PurchaseServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -60,6 +61,45 @@ class PurchaseServiceTest {
         // Verify
         verify(purchaseRepository, times(1))
                 .save(purchase);
+    }
+
+    @Test
+    public void testGetPurchaseById() {
+        // Given
+        Integer id = 1;
+
+        Customer customer = new Customer(
+                "John Doe",
+                "john@example.com"
+        );
+
+        Car car = new Car(
+                "Toyota", "Camry", 2021, 25000, null
+        );
+
+        Purchase purchase = new Purchase(
+                LocalDate.now(),
+                customer,
+                car
+        );
+
+        purchase.setId(id);
+
+        // Mocks
+        when(purchaseRepository.findById(id))
+                .thenReturn(Optional.of(purchase));
+
+        // When
+        Optional<Purchase> foundPurchase = purchaseService.getPurchaseById(id);
+
+        // Then
+        assertTrue(foundPurchase.isPresent());
+        assertEquals(customer, foundPurchase.get().getCustomer());
+        assertEquals(car, foundPurchase.get().getCarPurchased());
+
+        // Verify
+        verify(purchaseRepository, times(1))
+                .findById(id);
     }
 
     // Given
